da32afe386f094837e5807d2cdff3b5448c9fec8
Carrion-lab <69348873+Carrion-lab@users.noreply.github.com>
2023-11-02 15:24:45 +0100

    Update rename_genomes.R

--- src/rename_genomes.R
+++ src/rename_genomes.R
@@ -10,7 +10,7 @@ df <- data.frame(Full_name = files, Genus = sapply(strsplit(files,"_"), `[`, 1),
 
 df$Strain <- paste0( 'X',sprintf('%0.5d', 1:nrow(df)))
 df$Species <- str_remove(df$Species, '[.]')
-df$MicroLife_name <- paste0(df$Genus, '_', to_lower(str_remove_all(df$Species, '[.]')) , '_', df$Strain, '_O.fna')
+df$MicroLife_name <- paste0(df$Genus, '_', tolower(str_remove_all(df$Species, '[.]')) , '_', df$Strain, '_O.fna')
 
 file.rename(paste0('data/',df$Full_name),paste0( 'data/', df$MicroLife_name))
 
