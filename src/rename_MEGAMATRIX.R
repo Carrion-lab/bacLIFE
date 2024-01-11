27d59b05316fbcb5dc8232663cfb441edb82a8f0
Carrion-lab <69348873+Carrion-lab@users.noreply.github.com>
2024-01-11 17:58:10 +0100

    Update rename_MEGAMATRIX.R

--- src/rename_MEGAMATRIX.R
+++ src/rename_MEGAMATRIX.R
@@ -9,15 +9,15 @@ matrix <- as.data.frame(matrix)
 
 names_equivalence <- read.table(args[3], header = T)
 names_equivalence$Full_name <- str_remove(names_equivalence$Full_name, '_O.fna')
-names_equivalence$MicroLife_name <- str_remove(names_equivalence$MicroLife_name, '_O.fna')
+names_equivalence$bacLIFE_name <- str_remove(names_equivalence$bacLIFE_name, '_O.fna')
 
 n_samples <- grep("completeness", colnames(matrix)) - 1
 
-old_colnames <- data.frame(MicroLife_name = colnames(matrix)[2:n_samples] ) 
+old_colnames <- data.frame(bacLIFE_name = colnames(matrix)[2:n_samples] ) 
 
-M <- merge(old_colnames, names_equivalence, by = 'MicroLife_name')
+M <- merge(old_colnames, names_equivalence, by = 'bacLIFE_name')
 
-M <- M[match(M$MicroLife_name,old_colnames$MicroLife_name),]
+M <- M[match(M$bacLIFE_name,old_colnames$bacLIFE_name),]
 colnames(matrix)[2:n_samples] <- M$Full_name
 
 write.table(matrix, args[4], row.names = F)
@@ -27,11 +27,11 @@ write.table(matrix, args[4], row.names = F)
 big_scape_matrix <- read.table(args[2], header = T)
 
 
-old_colnames <- data.frame(MicroLife_name = colnames(big_scape_matrix) ) 
+old_colnames <- data.frame(bacLIFE_name = colnames(big_scape_matrix) ) 
 
-M <- merge(old_colnames, names_equivalence, by = 'MicroLife_name')
+M <- merge(old_colnames, names_equivalence, by = 'bacLIFE_name')
 
-M <- M[match(M$MicroLife_name,old_colnames$MicroLife_name),]
+M <- M[match(M$bacLIFE_name,old_colnames$bacLIFE_name),]
 colnames(big_scape_matrix) <- M$Full_name
 
 write.table(big_scape_matrix, args[5], row.names = T)
