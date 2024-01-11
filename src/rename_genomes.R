7dc94d1d374888ddd330472ba0e2f85678271304
Carrion-lab <69348873+Carrion-lab@users.noreply.github.com>
2024-01-11 17:57:14 +0100

    Update rename_genomes.R

--- src/rename_genomes.R
+++ src/rename_genomes.R
@@ -10,9 +10,9 @@ df <- data.frame(Full_name = files, Genus = sapply(strsplit(files,"_"), `[`, 1),
 
 df$Strain <- paste0( 'X',sprintf('%0.5d', 1:nrow(df)))
 df$Species <- str_remove(df$Species, '[.]')
-df$MicroLife_name <- paste0(df$Genus, '_', tolower(str_remove_all(df$Species, '[.]')) , '_', df$Strain, '_O.fna')
+df$bacLIFE_name <- paste0(df$Genus, '_', tolower(str_remove_all(df$Species, '[.]')) , '_', df$Strain, '_O.fna')
 
-file.rename(paste0('data/',df$Full_name),paste0( 'data/', df$MicroLife_name))
+file.rename(paste0('data/',df$Full_name),paste0( 'data/', df$bacLIFE_name))
 
 
-write.table(df[,c('Full_name', 'MicroLife_name')], 'names_equivalence.txt', row.names = F)
+write.table(df[,c('Full_name', 'bacLIFE_name')], 'names_equivalence.txt', row.names = F)
