441218337512f8250e8eb6be9dc3b91eed73ae2c
Guillermo Guerrero <55698504+gguerr001@users.noreply.github.com>
2024-11-06 16:42:49 +0100

    Update join_metadata.R

--- download/src/join_metadata.R
+++ download/src/join_metadata.R
@@ -1,5 +1,4 @@
 ##Script to join metadata
-names_equivalence = read.table('names_equivalence.txt', header = T)
 
 NCBIid = read.table('names_NCBIid.txt', header = F)
 NCBIid$V1 <- paste0(NCBIid$V1, '.fa')
@@ -9,11 +8,7 @@ clusters = read.table('cluster_membership.txt', header = T)
 
 
 M <- merge(clusters, NCBIid, by.x = 'Sample', by.y = 'V1')
-M <- merge(M, names_equivalence, by.x = 'V2', by.y = 'Full_name', all.x = T)
-colnames(M) <- c('scientific_name', 'NCBIid', 'cluster_membership', 'representative', 'MicroLife_name')
+colnames(M) <- c('NCBIid', 'cluster_membership', 'representative', 'scientific_name')
 
 write.table(M, 'METADATA_MERGED.txt', row.names = F, quote = F)
 
-##Create mapping_file
-mapping_file = data.frame(Sample = M$scientific_name, Lifestyle = 'Unknown')
-write.table(mapping_file, 'mapping_file.txt', row.names = F, quote = F)
