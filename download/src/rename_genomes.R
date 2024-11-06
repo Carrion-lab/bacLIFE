814acc48baa2d34319a0e1fd78076f56d1f4e5fd
Guillermo Guerrero <55698504+gguerr001@users.noreply.github.com>
2024-11-06 16:42:26 +0100

    Update rename_genomes.R

--- download/src/rename_genomes.R
+++ download/src/rename_genomes.R
@@ -14,11 +14,10 @@ new_names = new_names[new_names$V1 %in% files,]
 
 df <- data.frame(NCBIid =  new_names$V1 ,Full_name = new_names$V2, Genus = sapply(strsplit(new_names$V2,"_"), `[`, 1), Species = sapply(strsplit(new_names$V2,"_"), `[`, 2), Strain = sapply(strsplit(new_names$V2,"_"), `[`, 3) )
 
-df$Strain <- paste0( 'X',sprintf('%0.5d', 1:nrow(df)))
+
 
 df$bacLIFE_name <- paste0(df$Genus, '_', df$Species, '_', df$Strain, '_O.fna')
 
 file.rename(paste0('genomes_renamed/',df$NCBIid),paste0( 'genomes_renamed/', df$bacLIFE_name))
 
 
-write.table(df[,c('Full_name', 'bacLIFE_name')], 'names_equivalence.txt', row.names = F)
